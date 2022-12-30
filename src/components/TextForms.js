a8e2d0d0fa96fa80f0bcf571257dc2bedf9e6d9b
Rishi <rutupawar999@gmail.com>
2022-12-30 17:53:43 +0530

    initial adding hooks

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForms(props) {
+
+    const [text, setText] = useState('Enter text');
+    const handleOnChange = ()=>{
+        console.log(12);
+    }
+
+    const handleOnChange1 = ()=>{
+        console.log(13);
+    }
+
   return (
     <div>
         <form>
             <div className="form-group">
-                <label htmlFor='myTextBox'>{props.Title}</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                <h5>{props.Title}</h5>
+                <textarea className="form-control" id="myTextBox" value={text} onChange={handleOnChange} rows="5"></textarea>
+                <button className="btn btn-primary">Convert to uppercase</button>
             </div>
         </form>
     </div>
