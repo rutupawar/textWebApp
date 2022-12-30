c7d35b159e1585d39c19113b66a01ed334a15f41
Rishi <rutupawar999@gmail.com>
2022-12-30 18:55:18 +0530

    new button, and in script logic

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -2,27 +2,34 @@ import React, {useState} from 'react'
 
 export default function TextForms(props) {
 
-    const [text, setText] = useState('d');  // This is setting initial state for variable
+    const [text, setText] = useState('type here');  // This is setting initial state for variable
 
     const handleOnChange = (event)=>{   // event is populated here automatically when event is fired
         setText(event.target.value)
     }
 
-    const handleUpClick = (event)=>{   // event is populated here automatically when event is fired
+    const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
-        event.preventDefault();     // This will prevent the page from reloading
+    }
+
+    const handlelowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
     }
 
   return (
-    <div>
-        <form>
-            <div className="form-group">
-                <h5>{props.Title}</h5>
-                <textarea className="form-control" id="myTextBox" value={text} onChange={handleOnChange} rows="5"></textarea>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-            </div>
-        </form>
+    <>
+    <div className="container">
+        <h5>{props.Title}</h5>
+        <textarea className="form-control" id="myTextBox" value={text} onChange={handleOnChange} rows="5"></textarea>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handlelowClick}>Convert to lowercase</button>
     </div>
+    <div className='container my-2'>
+        <h6>Text Summary</h6>
+        <p>{text.split(' ').length} words and {text.length} characters</p>
+    </div>
+    </>
   )
 }
